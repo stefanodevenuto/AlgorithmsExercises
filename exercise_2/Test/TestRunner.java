a0eb4ebb8e5b1ce0d88ab56bd084d55940a48ae7
Stefano De Venuto <stefano.devenuto@edu.unito.it>
2020-04-13 10:21:41 +0200

    Adds Unit Tests for EditDistance class

---
+++ exercise_2/Test/TestRunner.java
@@ -0,0 +1,17 @@
+package Test;
+
+import org.junit.runner.JUnitCore;
+import org.junit.runner.Result;
+import org.junit.runner.notification.Failure;
+
+public class TestRunner {
+	public static void main(String[] args) {
+    	Result result = JUnitCore.runClasses(editDistanceTests.class);
+
+		if(result.wasSuccessful())
+			System.out.println("Tests completed successfully");
+		else
+			for (Failure failure : result.getFailures())
+        		System.out.println(failure.toString());
+   }
+} 
---
+++ exercise_2/Test/editDistanceTests.java
@@ -0,0 +1,59 @@
+package Test;
+
+import org.junit.*;
+import static org.junit.Assert.assertEquals;
+import Lib.*;
+
+public class editDistanceTests{
+
+	@Test
+	public void editDistanceEmptyStrings(){
+		assertEquals(EditDistance.editDistance("", ""), 0);
+	}
+
+	@Test
+	public void editDistanceFirstStringEmpty(){
+		assertEquals(EditDistance.editDistance("", "casa"), 4);
+	}
+
+	@Test
+	public void editDistanceSecondStringEmpty(){
+		assertEquals(EditDistance.editDistance("cassa", ""), 5);
+	}
+
+	@Test
+	public void editDistanceSameStrings(){
+		assertEquals(EditDistance.editDistance("pioppo", "pioppo"), 0);
+	}
+
+	@Test
+	public void editDistanceRandomString(){
+		assertEquals(EditDistance.editDistance("cassa", "casa"), 1);
+	}
+
+	@Test
+	public void editDistanceDynEmptyStrings(){
+		assertEquals(EditDistance.editDistanceDyn("", ""), 0);
+	}
+
+	@Test
+	public void editDistanceDynFirstStringEmpty(){
+		assertEquals(EditDistance.editDistanceDyn("", "casa"), 4);
+	}
+
+	@Test
+	public void editDistanceDynSecondStringEmpty(){
+		assertEquals(EditDistance.editDistanceDyn("cassa", ""), 5);
+	}
+
+	@Test
+	public void editDistanceDynSameStrings(){
+		assertEquals(EditDistance.editDistance("pioppo", "pioppo"), 0);
+	}
+
+	@Test
+	public void editDistanceDynRandomString(){
+		assertEquals(EditDistance.editDistanceDyn("cassa", "casa"), 1);
+	}
+
+}
